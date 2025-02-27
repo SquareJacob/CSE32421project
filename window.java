f7151b53bded99a550de58bfd54be1365262dc91
SquareJacob <jacobascherzer@gmail.com>
2025-02-27 16:53:02 -0500

    comments

--- window.java
+++ window.java
@@ -4,9 +4,9 @@ import javax.swing.JPanel;
 
 public class window{
     public static void main(String[] args){
-        JFrame frame = new JFrame("WE WINDOWS");
+        JFrame frame = new JFrame("WE WINDOWS"); //Where to set title of window
         JPanel panel = new JPanel();
-        String[] items = {"WE", "WIN", "DOWS"};
+        String[] items = {"WE", "WIN", "DOWS"}; //Ok but actually this is where the entities will go
         JComboBox<String> comboBox = new JComboBox<>(items);
         panel.add(comboBox);
         frame.add(panel);
