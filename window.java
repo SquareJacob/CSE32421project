d36ac787f8c46117675cccb493491b5ff3508b80
SquareJacob <jacobascherzer@gmail.com>
2025-02-27 16:40:43 -0500

    WE WINDOWS

--- window.java
+++ window.java
@@ -1,8 +1,8 @@
-import javax.swing.JFrame;
+import javax.swing.JFrame; 
 
 public class window{
     public static void main(String[] args){
-        JFrame frame = new JFrame("Simple Window");
+        JFrame frame = new JFrame("WE WINDOWS");
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
