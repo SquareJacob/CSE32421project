e373a157434e9379564a17a3b7209cbe49ce98d9
SquareJacob <jacobascherzer@gmail.com>
2025-02-27 16:49:05 -0500

    WE WIN DOWS

--- window.java
+++ window.java
@@ -1,8 +1,15 @@
-import javax.swing.JFrame; 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class window{
     public static void main(String[] args){
         JFrame frame = new JFrame("WE WINDOWS");
+        JPanel panel = new JPanel();
+        String[] items = {"WE", "WIN", "DOWS"};
+        JComboBox<String> comboBox = new JComboBox<>(items);
+        panel.add(comboBox);
+        frame.add(panel);
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
